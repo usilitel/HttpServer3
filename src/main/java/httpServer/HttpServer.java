a975f3f52c92a9db7527ad8a4420ab7635c0c515
user1 <usilitel00@gmail.com>
2017-07-27 00:33:11 +0300

    ServerSocket separated

---
+++ src/main/java/httpServer/HttpServer.java
@@ -0,0 +1,25 @@
+package httpServer;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+
+
+
+public class HttpServer {
+
+    public static void main(String[] args) throws Throwable {
+        ServerSocket ss = new ServerSocket(8080);
+        while (true) {
+            Socket s = ss.accept();
+            System.err.println("Client accepted");
+            new Thread(new SocketProcessor(s)).start();
+        }
+    }
+
+
+}
